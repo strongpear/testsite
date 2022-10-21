1f6d26aadc4d5c54cbc1642deb97498446e4cc8b
strongpear <jasonhclee1@gmail.com>
2022-10-21 18:14:47 -0500

    try again

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ const devConfig = {
 const proConfig = {
     connectionString: process.env.DATABASE_URL //heroku addons
 }
-const pool = new Pool(
-    process.env.NODE_ENV === "production" ? proConfig : devConfig
-)
+const pool = new Pool({
+    connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
+})
 
 // Function to register the user
 app.post('/register', (req, res) => {
