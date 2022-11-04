ae53ac86b9fbfa95b2380d95ec666c374b66ee83
strongpear <jasonhclee1@gmail.com>
2022-11-04 11:04:01 -0500

    Update AdminPage.js

--- client/src/components/pages/AdminPage.js
+++ client/src/components/pages/AdminPage.js
@@ -10,7 +10,7 @@ export default function AdminPage() {
 
     const getKYC = async () => {
         //const res = await Axios.get('/admin');
-        const res = Axios.get('/admin');
+        const res = await Axios.get('/admin');
         console.log(res)
         if (res.data.rows) {
             setKYC(res.data.rows);
