3b978725eea78bcce10b5b3c7a38cadd1fff255e
Jaspal95 <jaspal@tamu.edu>
2022-11-09 15:32:48 -0600

    Changed home page to test deploymeny in Heroku

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -13,7 +13,7 @@ export default function HomePage() {
     }
     return (
         <div className="text-center">
-            <h1 className="main-title home-page-title">welcome to our app</h1>
+            <h1 className="main-title home-page-title">welcome to jaspals app</h1>
             <Link to="/kyc">
                 <button className="primary-button">KYC Details</button>
             </Link>
