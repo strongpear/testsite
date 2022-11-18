bdb9c2e9ed08e528523f2ccffbbfdd18964376af
Jaspal95 <jaspal@tamu.edu>
2022-11-17 18:31:09 -0600

    cookie

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,8 +1,6 @@
-import Axios from "axios";
-import React from 'react'
 import { Link } from 'react-router-dom'
-import { Fragment, useEffect, useState } from "react";
-
+import Axios from "axios";
+import React, { Fragment, useEffect, useState } from "react";
 Axios.defaults.withCredentials = true
 
 export default function HomePage() {
@@ -10,6 +8,7 @@ export default function HomePage() {
 
     const getUsername = async () => {
         const res = await Axios.post('/home');
+        console.log(`This is the front end for the homepage`)
         console.log(res)
         setUsername(res);
     }
