76f34d4034d0611125054cff1a99da1d869c404c
Thao Hoang <94573189+thaothoang@users.noreply.github.com>
2022-12-02 11:18:01 -0600

    Updated payment testing

--- cucumberjs/test/features/step_definitions/SendTest.js
+++ cucumberjs/test/features/step_definitions/SendTest.js
@@ -4,22 +4,25 @@ const { By } = require("selenium-webdriver");
 setDefaultTimeout(60*1000)
 // const { default: builder } = require("cucumber/lib/formatter/builder");
 const { isAsyncFunction } = require("util/types");
+const { threadId } = require("worker_threads");
 
 Given('user is on the Payment page', async function () {
     return this.page.goto("https://ghostdynos.herokuapp.com/payment");
   });
 
-When('user enters username of the person they want to send money to and the amount', async function () {
-    
-
+When('user enters username of the person they want to send money to, the amount, and the message', async function () {
+    await this.driver.findElement(By.id("receiver")).sendKeys("username123");
+    await this.driver.findElement(By.id("amount")).sendKeys("50");
+    await this.driver.findElement(By.id("message")).sendKeys("testing");
     
   });
 
 And('user clicks on the Pay button', async function() {
-    await this.driver.findElement(By.linkText("Pay")).click();
+    await this.driver.findElement(By.id("sub_btn")).click();
   });
 
 Then('user is navigated to the home page', async function () {
-    const el = await this.page.waitForSelector("https://ghostdynos.herokuapp.com/home");
-    return !!el;
+    await this.driver.findElement(By.id("sub_btn")).isDisplayed();
+    driver.close();
+    driver.quit();
   });
