af5935c4b5c93ea238ac2697af787d423d89e9a0
Jaspal95 <jaspal@tamu.edu>
2022-12-01 21:39:57 -0600

    Authenitcaion

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ app.post('/logout', (req, res) => {
   console.log(req.session.user);
   //delete req.session.user;
   req.session.user = '';
-  console.log(Set session to an empty string);
+  console.log("Set session to an empty string");
   console.log(req.session.user);
 })
 
