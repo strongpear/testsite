645d56c09b22ea63ae7392dbb1ce11fba2cfcabb
Jaspal95 <jaspal@tamu.edu>
2022-12-02 10:01:51 -0600

    Regex stuff

--- client/src/components/pages/KYCPage.js
+++ client/src/components/pages/KYCPage.js
@@ -27,69 +27,93 @@ export default function KYCPage() {
     })
   }
   
-    return (
-        <div className="text-center m-5-auto">
-            <h2>Profile</h2>
-            <h5>Update your KYC details</h5>
-            <form action="/home">
-                <p>
-                    <label>First Name</label><br/>
-                    <input 
-                        type="text"
-                        id="firstname"
-                        onChange = {(e) => {setFirstname(e.target.value);}}
-                    />
-                </p>
-                <p>
-                    <label>Last Name</label><br/>
-                    <input 
-                        type="text"
-                        id="lastname"
-                        onChange = {(e) => {setLastname(e.target.value);}}
-                    />
-                </p>
-                <p>
-                    <label>Drivers License ID</label><br/>
-                    <input 
-                        type="text"
-                        id="driverid"
-                        onChange = {(e) => {setDriverID(e.target.value);}}
-                    />
-                </p>
-                <p>
-                    <label>State</label><br/>
-                    <input 
-                        type="text"
-                        id="state"
-                        onChange = {(e) => {setState(e.target.value);}} 
-                    /> 
-                </p>
-                <p>
-                    <label>ZIP</label><br/>
-                    <input 
-                        type="text"
-                        id="zip"
-                        onChange = {(e) => {setZip(e.target.value);}} 
-                    /> 
-                </p>
-                <p>
-                    <label>Birth Date</label><br/>
-                    <input 
-                        type="text"
-                        id="birthdate"
-                        onChange = {(e) => {setBirthDate(e.target.value);}}
-                    />
-                </p>
-                <p>
-                    <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all information is correct </span>.
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit" onClick={kycform}>Save</button>
-                </p>
-            </form>
-            <footer>
-                <p><Link to="/home">Back to Dashboard</Link>.</p>
-            </footer>
-        </div>
-    )
+  return (
+    <div className="text-center m-5-auto">
+        <h2>Profile</h2>
+        <h5>Update your KYC details</h5>
+        <form action="/home">
+            <p>
+                <label>First Name</label><br/>
+                <input 
+                    type="text"
+                    required
+                    pattern="[A-Za-z].{0,}"
+                    title="First name must be letters"
+                    id="firstname"
+                    onChange = {(e) => {setFirstname(e.target.value);}}
+                />
+            </p>
+            <p>
+                <label>Last Name</label><br/>
+                <input 
+                    type="text"
+                    required
+                    pattern="[A-Za-z].{0,}"
+                    title="Last name must be letters"
+                    id="lastname"
+                    onChange = {(e) => {setLastname(e.target.value);}}
+                />
+            </p>
+            <p>
+                <label>Drivers License ID</label><br/>
+                <input 
+                    type="text"
+                    required
+                    pattern="[0-9]{8}"
+                    title="Driver License ID must contain 8 digits"
+                    placeholder="12345678"
+                    id="driverid"
+                    onChange = {(e) => {setDriverID(e.target.value);}}
+                />
+            </p>
+            <p>
+                <label>State</label><br/>
+                <input 
+                    type="text"
+                    required
+                    pattern="[A-Za-z]{2}"
+                    title="State must contain 2 letters"
+                    placeholder="TX"
+                    id="state"
+                    onChange = {(e) => {setState(e.target.value);}} 
+                /> 
+            </p>
+            <p>
+                <label>ZIP</label><br/>
+                <input 
+                    type="text" 
+                    required
+                    pattern="[0-9]{5}"
+                    title="Zipcode must contain 5 digits"
+                    placeholder="77840"
+                    id="zip"
+                    onChange = {(e) => {setZip(e.target.value);}} 
+                /> 
+            </p>
+            <p>
+                <label>Birth Date</label><br/>
+                <input 
+                    type="text"
+                    required
+                    pattern="(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}"
+                    title="Birth Date must be in form of mm/dd/yyyy"
+                    placeholder="12/14/2022"
+                    id="birthdate"
+                    onChange = {(e) => {setBirthDate(e.target.value);}}
+                />
+            </p>
+            <p>
+                <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all information is correct </span>.
+            </p>
+            <p>
+                <button id="sub_btn" type="submit" onClick={kycform}>Save</button>
+            </p>
+        </form>
+
+        <footer>
+            <p><Link to="/home">Back to Dashboard</Link>.</p>
+        </footer>
+    </div>
+
+   )
 }
--- client/src/components/pages/RegisterPage.js
+++ client/src/components/pages/RegisterPage.js
@@ -21,46 +21,56 @@ export default function SignUpPage() {
     })
   }
   
-    return (
-        <div className="text-center m-5-auto">
-            <h2>Join us</h2>
-            <h5>Create your personal account</h5>
-            <form action="/login">
-                <p>
-                    <label>Username</label><br/>
-                    <input 
-                        type="text"
-                        id="usernameReg"
-                        onChange = {(e) => {setUsernameReg(e.target.value);}}
-                    />
-                </p>
-                <p>
-                    <label>Email address</label><br/>
-                    <input 
-                        type="text"
-                        id="emailReg"
-                        onChange = {(e) => {setEmailReg(e.target.value);}} 
-                    /> 
-                </p>
-                <p>
-                    <label>Password</label><br/>
-                    <input 
-                        type="password"
-                        id="passwordReg"
-                        onChange = {(e) => {setPasswordReg(e.target.value);}}
-                    />
-                </p>
-                <p>
-                    <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit" onClick={register}>Register</button>
-                </p>
-            </form>
-            <footer>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer>
-        </div>
+  return (
+    <div className="text-center m-5-auto">
+        <h2>Join us</h2>
+        <h5>Create your personal account</h5>
+        <form action="/login">
+            <p>
+                <label>Username</label><br/>
+                <input 
+                    type="text"
+                    required
+                    pattern=".{6,}"
+                    title="Username must contain 6 or more characters"
+                    placeholder="username123"
+                    id="usernameReg"
+                    onChange = {(e) => {setUsernameReg(e.target.value);}}
+                />
+            </p>
+            <p>
+                <label>Email address</label><br/>
+                <input 
+                    type="text"
+                    required
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                    placeholder="email@email.com"
+                    id="emailReg"
+                    onChange = {(e) => {setEmailReg(e.target.value);}} 
+                /> 
+            </p>
+            <p>
+                <label>Password</label><br/>
+                <input 
+                    type="password"
+                    required
+                    pattern=".{6,}"
+                    title="Password must contain 6 or more characters"
+                    id="passwordReg"
+                    onChange = {(e) => {setPasswordReg(e.target.value);}}
+                />
+            </p>
+            <p>
+                <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
+            </p>
+            <p>
+                <button id="sub_btn" type="submit" onClick={register}>Register</button>
+            </p>
+        </form>
+        <footer>
+            <p><Link to="/">Back to Homepage</Link>.</p>
+        </footer>
+    </div>
     )
 
 }
